858c4b9f311f5854dd3ad8846f7b6208a369945b
Francisca Medina <fbmedina@uc.cl>
2016-04-08 19:31:59 -0300

    Se construyó el modelo inventory con los campos de rueda, el serial_number no puede ser null

---
+++ app/models/inventary.rb
@@ -0,0 +1,2 @@
+class Inventary < ActiveRecord::Base
+end
---
+++ db/migrate/20160408222225_create_inventaries.rb
@@ -0,0 +1,11 @@
+class CreateInventaries < ActiveRecord::Migration
+  def change
+    create_table :inventaries do |t|
+      t.integer :serial_number
+      t.integer :wheel_size
+      t.text :wheel_description
+
+      t.timestamps null: false
+    end
+  end
+end
