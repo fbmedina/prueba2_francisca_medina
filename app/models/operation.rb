c69a7f4f0bd7c2e9c7fe31e18d06c6a37613909c
Francisca Medina <fbmedina@uc.cl>
2016-04-08 20:47:19 -0300

    se hizo el callback que sucede cuando se borra una operación

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,4 +2,10 @@ class Operation < ActiveRecord::Base
   belongs_to :worker
   has_one :inventary
 
+  before_destroy :register_deleted_operation
+
+  private
+    def register_deleted_operation
+      Register.create(description: 'La operación con referencia a la pieza #{self.inventary_id} y al usuario #{self.worker_id}, #{self.worker} ha sido borrada')
+    end
 end
