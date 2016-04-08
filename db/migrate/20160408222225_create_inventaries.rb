50b3bfe654e3bd0181115a51075aa9381c346fa4
Francisca Medina <fbmedina@uc.cl>
2016-04-08 19:35:32 -0300

    se trabajó sobre la migracion para hacer que el serial number no pueda ser null

--- db/migrate/20160408222225_create_inventaries.rb
+++ db/migrate/20160408222225_create_inventaries.rb
@@ -1,7 +1,7 @@
 class CreateInventaries < ActiveRecord::Migration
   def change
     create_table :inventaries do |t|
-      t.integer :serial_number
+      t.integer :serial_number, null: false
       t.integer :wheel_size
       t.text :wheel_description
 
