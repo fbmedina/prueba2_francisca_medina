4100cce08ade2bba048c57d1d8526e7281fe3404
Francisca Medina <fbmedina@uc.cl>
2016-04-08 21:20:15 -0300

    se creó scope que devuelve las operaciones hechas hace un día

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -4,6 +4,8 @@ class Operation < ActiveRecord::Base
 
   before_destroy :register_deleted_operation
 
+  scope :operations_performed_on_the_last_day, where('published_at >= ?', ime.now - 1.day)
+
   private
     def register_deleted_operation
       Register.create(description: 'La operación con referencia a la pieza #{self.inventary_id} y al usuario #{self.worker_id}, #{self.worker} ha sido borrada')
